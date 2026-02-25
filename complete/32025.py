f22750ab80183520afbc6cc305057fd1a80d7119
user <moojin0830@gmail.com>
2026-02-25 22:06:32 +0900

    feat: 문제풀이코드 추가 -브론즈5~실버5 사이의 문제 풀이 추가

---
+++ complete/32025.py
@@ -0,0 +1,3 @@
+a = int(input())
+b = int(input())
+print(int((a/2)*100)) if a<b else print(int((b/2)*100))
---
+++ complete/34921.py
@@ -0,0 +1,2 @@
+a,b = map(int,input().split())
+print(10+(2*(25-a+b))) if 10+(2*(25-a+b)) >=0 else print(0)
