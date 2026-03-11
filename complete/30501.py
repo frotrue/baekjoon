053af37d28a89d88cc6dc27610ea782b9541a322
mujin_laptop <moojin0830@gmail.com>
2026-03-11 12:21:47 +0900

    feat : 문제 풀이 추가

---
+++ complete/30501.py
@@ -0,0 +1,7 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    t= input()
+    if "S" in t:
+        print(t)
+        break
---
+++ complete/34813.py
@@ -0,0 +1,9 @@
+user = input()
+if user[0] == "F":
+    print("Foundation")
+elif user[0] == "C":
+    print("Claves")
+elif user[0] == "V":
+    print("Veritas")
+else:
+    print("Exploration")
