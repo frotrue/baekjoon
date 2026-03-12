dc63f48cd6d8ae132d38a5380d6c8fde66e78d98
mujin_laptop <moojin0830@gmail.com>
2026-03-12 12:18:58 +0900

    feat : 문제 풀이 추가

--- ongoing/1018.py
+++ ongoing/1018.py
@@ -1,23 +1,28 @@
-n,m = map(int,input().split())
-board = [[i for i in range(n)] for _ in range(m)]
-for i in range(n):
-    board[i] = list(map(str,input().split()))
+m,n = map(int,input().split())
+board = []
+for i in range(m):
+    board.append(list(input()))
 cnt =0
-if board[0][0][0] == "W":
-    for i in range(0,n):
-        for j in range(0,m):
-            if (j%2==0)and (i%2==0) and (board[i][j] == "W"):
-                continue
-            else:
-                cnt+=1
-elif board[0][0][0] == "B":
-    for i in range(0,n):
-        for j in range(0,m):
-            if (j%2==0)and (i%2==0) and (board[i][j] == "B"):
-                continue
-            else:
-                cnt+=1
+t = board[0]
+# correct_arr = []
+if t[0]=="W":
+    correct_arr = ['W' if i % 2 == 0 else 'B' for i in range(n)]
+    correct_arr2 = ['B' if i % 2 == 0 else 'W' for i in range(n)]
+else:
+    correct_arr = ['B' if i % 2 == 0 else 'W' for i in range(n)]
+    correct_arr2 = ['W' if i % 2 == 0 else 'B' for i in range(n)]
+for i in range(m):
+    if i%2==0 and board[i]==correct_arr[i]:
+        continue
+    elif i%2==1 and board[i]==correct_arr2[i]:
+        continue
+    else:
+        temp = board[i]
+        if i%2==0:
+            cnt +=sum(1 for a, b in zip(temp, correct_arr) if a != b)
+        if i%2==1:
+            cnt += sum(1 for a, b in zip(temp, correct_arr2) if a != b)
 
+# print(board)
 print(cnt)
-print(board[0][0][0])
-print(board)
+#TODO 일단 개수까지는 만들었는데 정사각형은 생각못하겠음. 나중에 재고해보기
